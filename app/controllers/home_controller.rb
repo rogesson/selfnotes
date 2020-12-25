475db808c5d87ac95aab6c3b5fad8201ce3de15d
rogesson <rogessonb@gmail.com>
2020-12-25 15:43:18 -0300

    create notes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
-    puts current_user.inspect
     if user_signed_in?
       @user_name = current_user.email.split('@')[0]
+      @notes = current_user.notes
     else
       @user_name = 'Guest'
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_214811) do
+ActiveRecord::Schema.define(version: 2020_12_25_161921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "important"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +33,10 @@ ActiveRecord::Schema.define(version: 2020_12_20_214811) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "notes", "users"
 end

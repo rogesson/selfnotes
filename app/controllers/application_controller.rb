efde84fc20fad85aef67e99df2e73b0d2c99fc2f
rogesson <rogessonb@gmail.com>
2020-12-26 13:13:20 -0300

    style notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,21 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :fetch_current_user
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:login, :email, :password)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:login, :email, :password, :current_password)}
+  end
+
+  def fetch_current_user
+    if user_signed_in?
+      @user_name = current_user.email.split('@')[0]
+      @notes = current_user.notes.reverse
+    else
+      @user_name = 'Guest'
+    end
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,4 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in?
-      @user_name = current_user.email.split('@')[0]
-      @notes = current_user.notes
-    else
-      @user_name = 'Guest'
-    end
   end
 end

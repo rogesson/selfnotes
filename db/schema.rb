30ece7955b7e7b332d9c250125c55d01f1ef7d4b
rogesson <rogessonb@gmail.com>
2020-12-27 19:54:09 -0300

    create notes scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_25_161921) do
+ActiveRecord::Schema.define(version: 2020_12_27_223428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
@@ -22,6 +28,8 @@ ActiveRecord::Schema.define(version: 2020_12_25_161921) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_notes_on_category_id"
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
@@ -33,10 +41,10 @@ ActiveRecord::Schema.define(version: 2020_12_25_161921) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "notes", "categories"
   add_foreign_key "notes", "users"
 end

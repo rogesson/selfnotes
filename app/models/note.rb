a61b536adc0557104c1c44472f5a4d7c7afbe74c
rogesson <rogessonb@gmail.com>
2021-01-03 16:58:59 -0300

    create partial to note card

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,16 @@ class Note < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
+  PRIORITIES = [1, 2, 3]
+
   #scope :priority, -> { where(important: true) }
   scope :priority, -> { where.not(priority: nil).order(:priority) }
+
+  def self.priorities_for_select
+    [
+      ["#1", 1],
+      ["#2", 2],
+      ["#3", 3]
+    ]
+  end
 end

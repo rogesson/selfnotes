a6da0340083f0caba093b10cf1f587d8c178d2c7
rogesson <rogessonb@gmail.com>
2021-01-17 13:35:14 -0300

    create note_by_priority test

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
+  describe '#by_priority' do
+    it 'returns a map of notes gruped by priority' do
+      notes = Note.all.priority
+
+      expect(notes).to eq([])
+    end
+  end
+
   describe '.priorities_for_select' do
     it do
       expect(1).to eq(1)
